ff9c9b58414dd716aec7506c7f92b18856ef9833
Kevin <kevinyeti@gmail.com>
2018-01-18 16:25:04 +0800

    WIP:add tests support

---
+++ src/AssemblyToReference/NormalClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AssemblyToReference
+{
+    public class NormalClass
+    {
+        public string AttributeA {
+            get {
+                string a = "1";
+                string b = "2";
+
+                return a + b;
+            }
+        }
+
+        public int AttributeB
+        {
+            get
+            {
+                char a = '1';
+                char b = '2';
+
+                return a + b;
+            }
+        }
+
+        public static decimal Calc(int a, int b)
+        {
+            Thread.Sleep(10);
+            return 3.14M;
+        }
+
+    }
+}
---
+++ src/Tests/UnitTest.cs
@@ -0,0 +1,27 @@
+﻿using Cache.Fody;
+using Fody;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class UnitTest
+    {
+        static TestResult testResult;
+
+        static UnitTest()
+        {
+            var weavingTask = new ModuleWeaver();
+            testResult = weavingTask.ExecuteTestRun("AssemblyToReference.dll");
+        }
+
+        [Fact]
+        public void ValidateHelloWorldIsInjected()
+        {
+            var type = testResult.Assembly.GetType("AssemblyToReference.NormalClass");
+            var instance = (dynamic)Activator.CreateInstance(type);
+
+            Assert.Equal("12", instance.AttributeA);
+        }
+    }
+}
